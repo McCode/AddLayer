c5ece505f749c1a27d87a23d5a7be4db714f9e86
McCode <jonnywoh@live.com>
2014-09-29 22:18:17 -0400

    Enhanced formatting, perhaps improved performance
    I made some changes that may or may not have improved performance.  I'm
    too lazy to test it.
    Also, I improved formatting in a way that ~~probably makes it worse~~ I
    like.
    Also, I renamed things.
    Also, there are still no comments.
    Also, there is still only one person to date who this has benefited from
    this project.  9 months ago.

--- AddLayer.py
+++ AddLayer.py
@@ -1,23 +1,25 @@
 inputs = (
        ("Surface block type:", "blocktype"),
-       ("Cover:", ("Only this type", "All but this type")),
-       ("Ignore block data?", False),
-       ("Cover block type:", "blocktype"))
+       ("Cover:"             , ("Only this type", "All but this type")),
+       ("Ignore block data?" , False),
+       ("Cover block type:"  , "blocktype"))
 
 def perform(level, box, options):
-    surfaceType     = options["Surface block type:"].ID
-    surfaceTypeData = options["Surface block type:"].blockData
-    coverType     = options["Cover block type:"].ID
-    coverTypeData = options["Cover block type:"].blockData
-    ignoreData = options["Ignore block data?"]
-    coverPolicy = (options["Cover:"] == "Only this type")
-
+    surfaceType = options["Surface block type:"].ID
+    surfaceData = options["Surface block type:"].blockData
+    coverType   = options["Cover block type:"  ].ID
+    coverData   = options["Cover block type:"  ].blockData
+    ignoreData  = options["Ignore block data?" ]
+    coverPolicy = options["Cover:"             ] == "Only this type"
+    
     for x in xrange(box.minx, box.maxx):
         for z in xrange(box.minz, box.maxz):
             for y in xrange(box.maxy, box.miny-1, -1):
-                if level.blockAt(x, y, z) != 0:
-                    if (level.blockAt(x, y, z) == surfaceType and (level.blockDataAt(x, y, z) == surfaceTypeData or ignoreData)) == coverPolicy:
+                blockType = level.blockAt(x, y, z)
+                blockData = level.blockDataAt(x, y, z)
+                if blockType != 0:
+                    if (blockType==surfaceType and (blockData==surfaceData or ignoreData)) == coverPolicy:
                         level.setBlockAt(x, y+1, z, coverType)
-                        level.setBlockDataAt(x, y+1, z, coverTypeData)
+                        level.setBlockDataAt(x, y+1, z, coverData)
                     break
 
